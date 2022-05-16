9d568df26b988d67c05388f0558bad877e6037fd
Lucas Duarte <lucashdp@outlook.com>
2022-05-15 22:04:32 -0300

    feat: adjust express file config
    Co-authored-by: Diego Costa <costaadiego1989@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const bodyParse = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParse = require("body-parser");
+const cors = require("cors");
+const routes = require("./v1/routes");
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,16 +11,16 @@ app.use(cors());
 app.use(bodyParse.json());
 app.use(bodyParse.urlencoded({ extended: false }));
 
-app.get('/', (_, res) => {
-    return res.status(200).send({
-        message: 'API: OK',
-        version: 'V1',
-    });
+app.get("/", (_, res) => {
+  return res.status(200).send({
+    message: "API: OK",
+    version: "V1",
+  });
 });
 
-require('./controllers')(app);
+routes(app);
 
 app.listen(PORT, () => {
-    const now = new Date();
-    console.log(`Listening on port ${PORT} at ${now.toLocaleString('pt-BR')}...`);
+  const now = new Date();
+  console.log(`Listening on port ${PORT} at ${now.toLocaleString("pt-BR")}...`);
 });
